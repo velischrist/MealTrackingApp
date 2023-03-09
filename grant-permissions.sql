e2ab69a10a58dc0d87cbf5f4bfbddc7ee8055759
sarahyun <sarahyun02@gmail.com>
2023-03-08 20:41:32 -0800

    Add files via upload

---
+++ grant-permissions.sql
@@ -0,0 +1,11 @@
+CREATE USER 'appadmin'@'localhost' IDENTIFIED BY 'adminpw';
+CREATE USER 'appclient'@'localhost' IDENTIFIED BY 'clientpw';
+
+GRANT ALL PRIVILEGES ON mealtrackerdb.* TO 'appadmin'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON mealtrackerdb.meals TO 'appclient'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON mealtrackerdb.recipe TO 'appclient'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON mealtrackerdb.ratings TO 'appclient'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON mealtrackerdb.goals TO 'appclient'@'localhost';
+
+FLUSH PRIVILEGES;
