a3ac8fbb76e5765a921a8400963ffc9acb8d8782
samirjohnson <56701692+samirjohnson@users.noreply.github.com>
2023-03-09 12:42:00 -0800

    Add files via upload
    Script to generate random usernames, emails, and passwords

---
+++ generate_random_users.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+n_username = 20
+n_email = 10
+n_password = 10
+num_users = 200
+
+username_list = []
+email_list = []
+password_list = []
+email_choice = ['@gmail.com', '@caltech.edu', '@yahoo.com', '@hotmail.com']
+
+i = 0
+while i < num_users:
+     username = ''.join(random.choices(string.ascii_lowercase + string.ascii_digits, k = n_username))
+     email = ''.join(random.choices(string.ascii_lowercase + string.ascii_digits, k = n_email)) + \
+             ''.join(random.choice(email_choice))
+     password = ''.join(random.choices(string.ascii_lowercase + string.ascii_digits, k = n_password))
+     if username not in username_list and email not in email_list:
+          username_list.append(username)
+          email_list.append(email)
+          password_list.append(password)
+          i += 1
