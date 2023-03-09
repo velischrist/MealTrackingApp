92251f42055f4c88f71464ca79cfcf73c4280c01
Sarah Yun <sarahyun@Sarahs-MacBook-Air.local>
2023-03-09 15:24:07 -0800

    need to clean up and document admin app

--- grant-permissions.sql
+++ grant-permissions.sql
@@ -6,7 +6,7 @@ CREATE USER 'appclient'@'localhost' IDENTIFIED BY 'clientpw';
 
 GRANT ALL PRIVILEGES ON mealtracker.* TO 'appadmin'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON mealtracker.meals TO 'appclient'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON mealtracker.recipe TO 'appclient'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON mealtracker.recipes TO 'appclient'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON mealtracker.ratings TO 'appclient'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON mealtracker.goals TO 'appclient'@'localhost';
 
